02086d88a87e80986b66f46b3ddba5d5fb4413b7
Isaac de Souza <isaac@quemais.com.br>
2021-10-11 10:04:18 -0300

    feat: cleaner controller routes

--- src/round-six/r6.controller.ts
+++ src/round-six/r6.controller.ts
@@ -3,7 +3,7 @@ import { RoundHelper } from './r6.helper';
 import { PlayerDTO } from './r6.players.dto';
 import { RoundOneService } from './r6.roundOne.service';
 import { RoundTwoService } from './r6.roundTwo.service';
-//40/1.02/1.5/2/2/2/2
+//40/1.6/1.5/2/2/2/2
 @Controller('/round')
 export class RoundsController{
 
@@ -11,25 +11,14 @@ export class RoundsController{
     @Post('/1')
     startRoundOne(@Body() data){
         let players: PlayerDTO[] = <PlayerDTO[]> data.players;
-        players = RoundHelper.fillRemainingPlayers(players);
-        console.log(players);
-        
-        let roundOne = new RoundOneService(players);
-        if(players.length > 40)
-            return {'erro':"O número máximo de players para o 1 round é de 40 pessoas."};
-        let results = roundOne.round();
-        return results;
+        return new RoundOneService(players).round();
     }
 
 
     @Post('/2')
     startRoundTwo(@Body() data){
         let players: PlayerDTO[] = <PlayerDTO[]> data.players;
-        let roundTwo = new RoundTwoService(players);
-        if(players.length < 20)
-            return {'erro':"O número mínimo de players para o 1 round é de 30 pessoas."};
-        let results = roundTwo.round();
-        return results;
+        return new RoundTwoService(players).round();
     }
 
 
