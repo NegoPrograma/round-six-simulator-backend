3d2c813b7bf078e7542a1fa2af34f61077b171af
Isaac de Souza <isaac@quemais.com.br>
2021-10-11 10:01:43 -0300

    feat: update fill players algorithm

--- src/round-six/r6.helper.ts
+++ src/round-six/r6.helper.ts
@@ -14,16 +14,27 @@ export class RoundHelper {
         return length - 1;
     }
 
-    static fillRemainingPlayers(players: PlayerDTO[]) {
-        let totalIncrease = 40 - players.length;
-        let npcPlayer: PlayerDTO = <PlayerDTO> {
-            name: "NPC",
-            imageURL: "https://cf.shopee.com.br/file/9c33e5475f7b2f600a76aba997b90ba7"
-        }
-        for(let i = 0; i < totalIncrease;i++){
+    static fillRemainingPlayers(players: PlayerDTO[],max_players_for_this_round) {
+        let missingPlayers = max_players_for_this_round - players.length;
+        
+        for(let i = 0; i < missingPlayers;i++){
+            let npcPlayer: PlayerDTO = <PlayerDTO> {
+                name: "NPC " + i,
+                imageURL: "https://cf.shopee.com.br/file/9c33e5475f7b2f600a76aba997b90ba7"
+            }
             players.push(npcPlayer);
         }
         return players;
+    }
+
+    static shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
     }
     //static numberOfMiniRounds()
 }
